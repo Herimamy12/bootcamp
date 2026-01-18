de99618e1c4b8d9c3204d83b2ab9afb09cd00111
nherimam <herimaminavalonaprosper@gmail.com>
2026-01-18 15:20:17 +0300

    App.tsx test

--- react/meteo/src/App.tsx
+++ react/meteo/src/App.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react"
+import { WeatherForm } from "./components/WeatherForm"
+import { WeatherCard } from "./components/WeatherCard"
+import type { WeatherData } from "./api/weatherApi"
+
 function App() {
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+
   return (
-    <div>
-      <h1 className="text underline">Hello, Meteo App!</h1>
+    <div className="min-h-screen bg-base-100 flex flex-col items-center pt-10">
+      <h1 className="text-4xl font-bold mb-6">🌦️ Météo App</h1>
+
+      {/* Formulaire de recherche */}
+      <WeatherForm onWeatherFetched={setWeatherData} />
+
+      {/* Affichage de la météo si disponible */}
+      {weatherData ?
+      (
+        <WeatherCard data={weatherData} />
+      ) : (
+        <p className="mt-6 text-gray-600">Entrez une ville pour obtenir la météo.</p>
+      )}
     </div>
   )
 }
