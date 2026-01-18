afbb0577d3177ecc97ad8fe24150b6da8e31e654
nherimam <herimaminavalonaprosper@gmail.com>
2026-01-18 15:54:37 +0300

    Ville par defaut: Antananarivo

--- react/meteo/src/App.tsx
+++ react/meteo/src/App.tsx
@@ -1,12 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { WeatherForm } from "./components/WeatherForm"
 import { WeatherCard } from "./components/WeatherCard"
 import { Loader } from "./components/Loader"
 import type { WeatherData } from "./api/weatherApi"
+import { fetchWeather } from "./api/weatherApi"
 
 function App() {
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
   const [loading, setLoading] = useState(false);
+  const defaultCity = "Antananarivo";
+
+  useEffect(() => {
+    const fetchDefaultWeather = async () => {
+      setLoading(true);
+      try {
+        const data = await fetchWeather(defaultCity);
+        setWeatherData(data);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données météo par défaut:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDefaultWeather();
+  }, []);
 
   return (
     <div className="min-h-screen bg-base-100 flex flex-col items-center pt-10">
